bd4ab8e5ff9c413480c39440669ec6108f296a0d
Maciej Myśliński <maciej.myslinski@netguru.co>
2018-04-26 11:40:51 +0200

    Implement with react-scroll

--- src/scenes/Home/components/Menu/components/Item/components/Rectangle/index.js
+++ src/scenes/Home/components/Menu/components/Item/components/Rectangle/index.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Rectangle = styled.div`
   height: 24px;
   width: 165px;
-  background: ${({ active }) => (active ? '#73BF9' : '#c4c4c4')};
+  background: ${({ active }) => (active ? '#73BF93' : '#c4c4c4')};
   border-radius: 4px;
 `;
